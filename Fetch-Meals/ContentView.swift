1614e66131b02a866bcee53df9b307d088e2e0d6
Thinh Nguyen <nicknguyenios14@gmail.com>
2024-09-09 14:00:17 -0400

    Initial Commit

---
+++ Fetch-Meals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch-Meals
+//
+//  Created by Thinh Nguyen on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch-Meals/Fetch_MealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_MealsApp.swift
+//  Fetch-Meals
+//
+//  Created by Thinh Nguyen on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_MealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
