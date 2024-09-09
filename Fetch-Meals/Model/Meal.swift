d9d1488e718d7515864e9ad45a482277ccc2cfb3
Thinh Nguyen <nicknguyenios14@gmail.com>
2024-09-09 14:05:31 -0400

    Add Meal Codable model

---
+++ Fetch-Meals/Model/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  Fetch-Meals
+//
+//  Created by Thinh Nguyen on 9/9/24.
+//
+
+import Foundation
+
+struct Meal: Codable {
+    let strMeal: String
+    let strMealThumb: String
+    let idMeal: String 
+}
